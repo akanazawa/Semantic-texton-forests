b2d17816b2e4de2aa9108c0baf3211fa97f114af
akanazawa <kanazawa@umiacs.umd.edu>
2012-06-25 17:36:51 -0400

    change do_stf back to the first config with MSRC

--- do_stf.m
+++ do_stf.m
@@ -5,9 +5,8 @@
 % based on C# code provided by Matthew Johnson
 %
 %%%%%%%%%%%%%%%%%%%%
-%config_file = 'config';
-config_file = 'configML'; % doing MSRC and labelme subset together
-
+config_file = 'config';
+%config_file = 'configML'; % doing MSRC and labelme subset together
 
 %% train the forest
 do_train(config_file);
@@ -17,11 +16,11 @@ do_test(config_file);
 
 %% example: extract BOST
 % an image of cat from MSRC
-% I = imread('/Users/kanazawa/Documents/projects/datasets/MSRC21/Images/15_21_s.bmp');
-% gt = imread(['/Users/kanazawa/Documents/projects/datasets/MSRC21/' ...
-%              'GroundTruth/15_21_s_GT.bmp']);
-% % mask cuts out the cat
-% mask = logical(rgb2gray(gt)); 
-% % load trained 'forest'
-% load('results/forestFilled.mat'); 
-% bost = extractBost(I, mask, forest, 15);
+I = imread('/Users/kanazawa/Documents/projects/datasets/MSRC21/Images/15_21_s.bmp');
+gt = imread(['/Users/kanazawa/Documents/projects/datasets/MSRC21/' ...
+             'GroundTruth/15_21_s_GT.bmp']);
+% mask cuts out the cat
+mask = logical(rgb2gray(gt)); 
+% load trained 'forest'
+load('results/forestFilled.mat'); 
+bost = extractBost(I, mask, forest, 15);
